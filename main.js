4d83d4fd05648015311775e5ae84ba0177240a8a
52lions06 <mckelvey.william@yahoo.com>
2017-09-06 15:19:53 -0400

    added .remove(key) needs _replaceWith and _findMin()

--- main.js
+++ main.js
@@ -32,17 +32,46 @@ class BinaryTree {
 
   get(key) {
     if (key === this.key) {
-    //   console.log('first stop', key);
+      //   console.log('first stop', key);
       return this.value;
-        }
+    }
     else if (key < this.key && this.left) {
       return this.left.get(key);
-    //   console.log('next stop');
+      //   console.log('next stop');
     }
     else if (key > this.key && this.right) {
       return this.right.get(key);
-    //   console.log('second stop', key);
-    //   console.log('third stop', this.key);
+      //   console.log('second stop', key);
+      //   console.log('third stop', this.key);
+    }
+    else {
+      throw new Error('Key Error');
+    }
+  }
+
+  remove(key) {
+    if (key === this.key) {
+      if (this.left && this.right) {
+        const successcor = this.right._findMin();
+        this.key = successcor.key;
+        this.value = successcor.value;
+        successcor.remove(successcor.key);
+      }
+      else if (this.left) {
+          this.left._replaceWith();
+      }
+      else if (this.right) {
+          this.right._replaceWith();
+      }
+      else {
+          _replaceWith = null; 
+      }
+    }
+    else if (key < this.key && this.left) {
+      this.left.remove(key);
+    }
+    else if (key > this.key && this.right) {
+      this.right.remove(key);
     }
     else {
       throw new Error('Key Error');
